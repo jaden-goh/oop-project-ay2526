f6c7a2563077ee81c37263e64444354583fa4d6f
Jaden Goh <JGOH164@e.ntu.edu.sg>
2025-10-26 00:57:52 +0800

    base code

--- User.java
+++ User.java
@@ -9,6 +9,13 @@ public abstract class User {
         this.name = name;
         this.password = password;
     }
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) {
+        // if (name == null || name.isBlank()) throw new IllegalArgumentException("name required");
+        // to be added later, returning any exceptions good for edge cases
+        this.name = name;
+    }
 
     public boolean login() { return true; }
     public boolean verifyPassword(String password) { return true; }
