a5a88e6322c519440cf638c4854c5e90b7593c32
Yunqing <liuyunqing2003@gmail.com>
2025-11-08 19:14:18 +0800

    Updated Application Class

--- src/entity/Application.java
+++ src/entity/Application.java
@@ -1,17 +1,31 @@
 package entity;
 import java.util.Date;
+import java.util.Objects;
 
 public class Application {
     private String applicationID;
-    private String status;
+    private String status = "Pending";  // default pending
     private Date dateApplied;
     private boolean accepted;
     private boolean withdrawn;
     private Student student;
     private Internship internship;
 
-    public Application() {}
+    //public Application() {}
 
+    // constructor class
+    public Application(String applicationID, Student student, Internship internship, Date dateApplied) {
+        this.applicationID = Objects.requireNonNull(applicationID, "Application ID cannot be null");
+        this.student = Objects.requireNonNull(student, "Student cannot be null");
+        this.internship = Objects.requireNonNull(internship, "Internship cannot be null");
+        this.dateApplied = Objects.requireNonNull(dateApplied, "Date applied cannot be null");
+        this.status = "Pending";
+        this.accepted = false;
+        this.withdrawn = false;
+    }
+
+
+    // Getters and Setters
     public String getApplicationID() { return applicationID; }
     public void setApplicationID(String applicationID) { this.applicationID = applicationID; }
 
@@ -34,9 +48,28 @@ public class Application {
     public void setInternship(Internship internship) { this.internship = internship; }
 
     // functions
-    public void updateStatus(String newStatus) { }
-    public void markAccepted() { }
-    public void requestWithdrawal() { }
-    public boolean isPending() { return false; }
+
+    // updates status + error handling
+    public void updateStatus(String newStatus) {
+        if (newStatus == null || newStatus.isBlank()) return;
+        this.status = newStatus;
+    }
+
+    // Accepts only if status = accepted
+    public void markAccepted() {
+        this.accepted = true;
+    }
+
+    public boolean requestWithdrawal() {
+        if (!withdrawn){
+            withdrawn = true;
+            return true;
+        }
+
+        return false;
+    }
+    public boolean isPending() {
+        return "Pending".equalsIgnoreCase(status);
+    }
 }
 
